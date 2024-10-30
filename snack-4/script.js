66feb095465051f6f23c7d7d8aa37fe01c628428
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-30 15:59:43 +0100

    add fourth solution

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nameArray = posts.map(({author})=> author)
+console.log(nameArray)
