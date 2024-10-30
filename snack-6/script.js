25131164cc0784c2057c30afed1a3e2c739d9159
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-30 16:21:29 +0100

    create two array with the value

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,15 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const arrPlus15 = zucchine.filter(({length})=>{
+  console.log(length);
+  if(length>15){
+    return true
+}});
+
+const arrMinus15 = zucchine.filter(({length})=>{
+  if(length<=15){
+    return true
+}});
+
