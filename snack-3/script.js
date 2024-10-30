3bf1b9ee018ee40c59f7e5d414a4a3e34512e040
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-30 15:56:00 +0100

    add another solution for the request

--- snack-3/script.js
+++ snack-3/script.js
@@ -4,3 +4,13 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Risultato: [3, 9, 5, 8, 3, 88]
 const arr = numbers.map((num)=> num+1)
 console.log(arr)
+
+// let arrV2 = [];
+// let i=0;
+// numbers.forEach(
+//     (el)=> {
+//         arrV2[i]=  el+1
+//         i++;
+//     }
+// )
+// console.log(arrV2)
