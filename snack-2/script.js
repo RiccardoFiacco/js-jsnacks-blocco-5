9aa8b8b21ff799aa16b775f2cd81f2476ffc4d18
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-30 15:39:20 +0100

    add second solution

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,5 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+people.forEach(({name})=>console.log(name))
+//people.forEach((el)=>console.log(el.name))
